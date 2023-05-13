6ca88fb56f895a7f12275224cd3c50b1a5405d95
unknown <karma.andrew42@gmail.com>
2023-05-13 23:05:00 +0200

    setted up tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1>sal</h1>
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   )
 }

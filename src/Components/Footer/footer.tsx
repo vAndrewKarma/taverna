9e74b8dff31adf750cf17cc25014ecc3bb39c242
Karma Andrew <karma.andrew16@gmail.com>
2023-05-14 19:32:23 +0300

    fixed footer

--- src/Components/Footer/footer.tsx
+++ src/Components/Footer/footer.tsx
@@ -7,10 +7,7 @@ import Link from "next/link"
 const Footer = () => {
   return      <footer className="hidden sm:flex   text-2xl fixed lg:text-4xl align-bottom text-green-600 font-bold leading-[90%] tracking-[2px] font-sans py-4 px-6 sm:px-12 md:px-16 lg:px-20 xl:px-24 flex flex-col items-center justify-center   bottom-0 w-full">
 
-  <motion.p initial={{ y: 100, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                whileHover={{scale:0.80, transition:{duration:0.2}}}
-                transition={{ delay: 1.5, duration: 2, type: "spring", stiffness: 120 }} className="mb-5">Social Media</motion.p>
+  
 
 <div className="flex items-center gap-10">
 
